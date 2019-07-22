e65837f355d911d2e0670ad321aab8dd1f4ddfad
saikrishnapsk <sai.krishna.padarthi@rockwellcollins.com>
2019-07-22 11:10:43 +0530

    reports-checking-deviceusage

--- src/app/new-service.service.ts
+++ src/app/new-service.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Rx";
 import { Http, Headers, RequestOptions } from "@angular/http";
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class NewServiceService {
@@ -40,7 +41,7 @@ export class NewServiceService {
     // const url= 'http://services.groupkt.com/state/get/IND/all';
     return this.http
       .get(`${this.apiEndPoint}/IND`)
-      .map(function(res) {
+      .map(function (res) {
         console.log('res :', res);
         const data = res.json();
         return data;
@@ -53,6 +54,37 @@ export class NewServiceService {
     // const url = 'http://services.groupkt.com/country/get/all';
     const url = `${this.apiEndPoint}/IND/${code}`
     return this.http.get(url).map(res => {
+      const user = res.json();
+      return user;
+    });
+  }
+
+  getAuthToken(): Observable<any> {
+    const url = `http://localhost:8082/authenticate`;
+    let requestBody = {
+      "userName": "sandbox.psk1234",
+      "password": "Password$2"
+    }
+    return this.http.post(url, requestBody).map(res => {
+      const user = res.json();
+      return user;
+    });
+  }
+
+  getDeviceUsageDetails(jwt): Observable<any> {
+    console.log('jwt :', jwt);
+    let headers = new Headers();
+    headers.append('Authorization', `Bearer ${jwt}`);
+    let options = new RequestOptions({ headers: headers });
+    let requestBody = {
+      "workstationName": "YYZ1CN02",
+      "iataCode": "YYZ",
+      "startDate": "2017-02-23 04:00:30",
+      "endDate": "2017-02-25 04:00:30",
+      "accountingCode": "176"
+    }
+    const url = `http://localhost:8082/api/deviceusage`;
+    return this.http.post(url, requestBody, options).map(res => {
       const user = res.json();
       return user;
     });
--- src/app/states/states.component.ts
+++ src/app/states/states.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, PipeTransform, Pipe  } from '@angular/core';
+import { Component, OnInit, PipeTransform, Pipe } from '@angular/core';
 import { Router } from '@angular/router';
-import {FormGroup, FormControl} from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 import { NewServiceService } from '../new-service.service';
-import {OrderByPipe} from '../order-by.pipe';
+import { OrderByPipe } from '../order-by.pipe';
 
 @Component({
   selector: 'app-states',
@@ -19,33 +19,48 @@ export class StatesComponent implements OnInit {
   public searchText;
   public resultSet;
   public resultSetCount;
+  public jwt:string;
+  public deviceUsageDetails: any;
   constructor(private router: Router, private myService: NewServiceService) { }
   searchForm = new FormGroup({
     search: new FormControl()
   })
- 
-  ngOnInit(){
+
+  ngOnInit() {
     // this.toggleValue = null;
     this.togggleUsing = 'capital';
-      this.myService.getStates().subscribe(
-        res => {
-          console.log(res);
-          this.resultSet = res.RestResponse.result;
-          this.resultSetCount = this.resultSet.length;
+    // this.myService.getStates().subscribe(
+    //   res => {
+    //     console.log(res);
+    //     this.resultSet = res.RestResponse.result;
+    //     this.resultSetCount = this.resultSet.length;
+    //   }
+    // );
+
+    this.myService.getAuthToken().subscribe(res => {
+      console.log('res :', res);
+      this.jwt = res.jwt;
+      this.myService.getDeviceUsageDetails(res.jwt).subscribe(
+        response => {
+          this.deviceUsageDetails  = response;
+          console.log("response : ", response);
         }
       );
+      return res.jwt;
+    });
+    
   }
-  getSomeDetails(item){
+  getSomeDetails(item) {
     // console.log('item', item)
-    this.router.navigate(['state',item.abbr])
+    this.router.navigate(['state', item.abbr])
   }
-  sortyBy(event){
+  sortyBy(event) {
     console.log(event)
     this.toggleButton = !this.toggleButton;
-    if(this.toggleButton){
+    if (this.toggleButton) {
       this.toggleValue = 'capital';
       this.togggleUsing = 'name';
-    }else {
+    } else {
       this.toggleValue = 'name';
       this.togggleUsing = 'capital';
     }
